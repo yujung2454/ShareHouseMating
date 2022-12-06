fe167a22ea654bb10e9e4bca66f94da20272d0c8
so <rachu970605@gmail.com>
2022-12-06 11:54:36 +0900

    12.06

--- ShareHouse/src/main/java/com/sharehouse/controller/TestController.java
+++ ShareHouse/src/main/java/com/sharehouse/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
 
 	@GetMapping("/viewtest")
-	public String viewtest() {
+	public String viewtest() {    
 		return "viewtest";
 	}
 }
