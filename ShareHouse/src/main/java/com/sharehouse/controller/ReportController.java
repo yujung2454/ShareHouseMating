c8a8850549ba8357179043362cfccae78f3e3344
soldesk <soldesk@DESKTOP-1AS6FQJ>
2022-12-29 17:49:46 +0900

    baesoeun

--- ShareHouse/src/main/java/com/sharehouse/controller/ReportController.java
+++ ShareHouse/src/main/java/com/sharehouse/controller/ReportController.java
@@ -1,5 +1,32 @@
 package com.sharehouse.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sharehouse.config.SecurityUser;
+import com.sharehouse.dto.ReportDto;
+import com.sharehouse.service.ReportService;
+
+@Controller
 public class ReportController {
 
+	@Autowired
+	ReportService service;
+	
+	@GetMapping("/report_maemul")
+	public String report_maemul() {
+		return "/report/report_maemul";
+	}
+	
+	@PostMapping("/report_maemul")
+	public String insertMaemul(@AuthenticationPrincipal SecurityUser user, ReportDto dto, int board_no) {
+		String Id = user.getUsers().getId();
+		dto.setReporter(Id);
+		service.insertReport(dto);
+		service.selectIdNo(board_no);
+		return "/offer/detail_info";
+	}
 }
--- ShareHouse/src/main/java/com/sharehouse/dao/ReportDao.java
+++ ShareHouse/src/main/java/com/sharehouse/dao/ReportDao.java
@@ -1,5 +1,20 @@
 package com.sharehouse.dao;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.sharehouse.dto.OfferingDto;
+import com.sharehouse.dto.ReportDto;
+
+@Mapper
 public interface ReportDao {
 
+@Insert
+("insert into user_report_board(reporter, reported_id, category, report_con, board_no) values(#{reporter}, #{reported_id}, #{category}, #{report_con}, #{board_no})")
+int insertReport(ReportDto dto);
+
+@Select
+("select id, board_no from offering where board_no = #{board_no}")
+OfferingDto selectIdNo(int board_no);	
 }
